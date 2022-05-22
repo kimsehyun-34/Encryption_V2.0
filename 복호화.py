eabff86cbd23cafb52062b613aaafab3b2f1bd61
FURY312 <79848348+FURY312@users.noreply.github.com>
2022-05-22 20:12:01 +0900

    Encryption V2.0
    -난이도 또한 암호화 처리함 (난이도*9567)
    -복호화 작업시 난이도 입력 필요없음.

--- 복호화.py
+++ 복호화.py
@@ -1,5 +1,8 @@
-na= int(input("난이도: "))
-kegm = input("암호문: ")
+import os
+maa= input("암호문: ")
+ba=maa.split()
+kegm=ba[0]
+na=int(int(ba[1])/9567) #9567
 
 kegm_s=""
 kegm2=""
@@ -12,3 +15,4 @@ for i in range(len(kegm)):
     c=chr(ac)
     kegm_s+=c
 print("해독 결과: ",kegm_s)
+os.system('pause')
--- 암호생성.py
+++ 암호생성.py
@@ -1,6 +1,7 @@
+import os
 kegm = input("문장: ")
-na = int(input("난이도(~600): ")) #아스키 코드 증가값 조절가능 (난이도)
-
+na = int(input("난이도(~600): ")) #아스키 코드 증가값 조절가능
+ma=9567
 kegm_s=""
 kegm2=""
 c=''
@@ -8,9 +9,10 @@ ac=0
 for i in range(len(kegm)):
     c=kegm[i]
     ac=ord(c)
-    ac+=na #아스키 코드 +30 
+    ac+=na #아스키 코드+
     c=chr(ac)
     kegm_s+=c
 
-print("암화 결과: ",kegm_s)
-print("난이도 :",na)
+naa=na*ma
+print("암호화 결과: ",kegm_s,naa)
+os.system('pause')
