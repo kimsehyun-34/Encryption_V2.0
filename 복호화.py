0191dcc77e7eeb705da1ee1f1ab64f3d00d79243
FURY <79848348+FURY312@users.noreply.github.com>
2023-05-14 15:18:48 +0900

    Update 복호화.py

--- 복호화.py
+++ 복호화.py
@@ -8,11 +8,11 @@ kegm_s=""
 kegm2=""
 c=''
 ac=0
-for i in range(len(kegm)):
+for i in range(len(kegm)): #아스키코드 변경작업
     c=kegm[i]
     ac=ord(c)
     ac-=na
     c=chr(ac)
     kegm_s+=c
 print("해독 결과: ",kegm_s)
-os.system('pause')
+os.system('pause') #모든 코드가 출력후 자동좋료르 방지 하기위해서 추가함
