98e01b6b2d31c0b25a6f7c9a1959d0ee70f9b301
FURY <79848348+FURY312@users.noreply.github.com>
2023-05-14 15:24:49 +0900

    Update 암호생성.py

--- 암호생성.py
+++ 암호생성.py
@@ -1,18 +1,17 @@
 import os
 kegm = input("문장: ")
-na = int(input("+α 난이도(1~4200): ")) #아스키 코드 증가값 조절가능
-ma=95673
+na = int(input("+α 난이도(1~4200): ")) #아스키코드 증가값
+ma=95673 #복호화에 사용할 아스키코드 증가값을 암호화 하기위한 수(변경가능)
 kegm_s=""
-kegm2=""
 c=''
 ac=0
-for i in range(len(kegm)):
+for i in range(len(kegm)): #아스키코드 변경작업
     c=kegm[i]
     ac=ord(c)
-    ac+=na #아스키 코드+
+    ac+=na
     c=chr(ac)
     kegm_s+=c
 
 naa=na*ma
 print("암호화 결과: ",kegm_s,naa)
-os.system('pause')
+os.system('pause') #자동좋료르 방지 하기위해서 추가함
