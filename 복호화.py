05178300d483047d17a2c03c523c40ce9317228b
FURY <79848348+FURY312@users.noreply.github.com>
2023-05-14 15:25:35 +0900

    Update 복호화.py

--- 복호화.py
+++ 복호화.py
@@ -14,4 +14,4 @@ for i in range(len(kegm)): #아스키코드 변경작업
     c=chr(ac)
     kegm_s+=c
 print("해독 결과: ",kegm_s)
-os.system('pause') #모든 코드가 출력후 자동좋료르 방지 하기위해서 추가함
+os.system('pause') #자동좋료르 방지 하기위해서 추가함
