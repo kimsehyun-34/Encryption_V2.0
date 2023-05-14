74cc001a055eddddf569df50035c40dc62b95b61
FURY <79848348+FURY312@users.noreply.github.com>
2023-05-14 15:21:12 +0900

    Update 복호화.py

--- 복호화.py
+++ 복호화.py
@@ -1,11 +1,10 @@
 import os
 maa= input("암호문: ")
-ba=maa.split()
-kegm=ba[0]
-na=int(int(ba[1])/95673) #95673
+ba=maa.split() #입력한 암호문을 공백을 기준으로 나누어 저장함
+kegm=ba[0] #저장한 변수중 0번 변수를 kegm에 저장함
+na=int(int(ba[1])/95673) #암호키 계산 작업,95673로 나누어 계산함
 
 kegm_s=""
-kegm2=""
 c=''
 ac=0
 for i in range(len(kegm)): #아스키코드 변경작업
